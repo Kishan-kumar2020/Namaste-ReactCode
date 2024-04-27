51c80c6a259747cb931964389920dae142caa418
kishan.kumar <kishan.kumar@pragmaticplay.com>
2024-04-27 09:20:57 +0530

    Episode 1 completed Inception

--- App.js
+++ App.js
@@ -1,23 +1,23 @@
-// const heading = React.createElement("div", {id: "parent"},
-//             [
-//                 React.createElement("div", {id: "child"},
-//                 [
-//                     React.createElement("h1", {}, "I'm a h1 tag from React"),
-//                     React.createElement("h2", {}, "I'm a h2 tag from React")
-//                 ]),
-//                 React.createElement("div", {id: "child"},
-//                 [
-//                     React.createElement("h1", {}, "I'm a h1 tag from React"),
-//                     React.createElement("h2", {}, "I'm a h2 tag from React")
-//                 ])
-//             ]
-//         );
+const heading = React.createElement("div", {id: "parent"},
+            [
+                React.createElement("div", {id: "child"},
+                [
+                    React.createElement("h1", {}, "I'm a h1 tag from React"),
+                    React.createElement("h2", {}, "I'm a h2 tag from React")
+                ]),
+                React.createElement("div", {id: "child"},
+                [
+                    React.createElement("h1", {}, "I'm a h1 tag from React"),
+                    React.createElement("h2", {}, "I'm a h2 tag from React")
+                ])
+            ]
+        );
         
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+// const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "HELLO WORLD FROM REACT");
+// // const heading = React.createElement("TagName", {attributes}, {content inside the Tag // children});
+// console.log(heading);
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
-
-        const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "HELLO WORLD FROM REACT");
-        // const heading = React.createElement("TagName", {attributes}, {content inside the Tag // children});
-        console.log(heading);
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading);
